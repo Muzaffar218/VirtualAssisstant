7adc515bc5f20de893be25d1909b83d8888f38dd
Muzaffar218 <mohdmuzaffar.ahmed@dxc.com>
2020-10-19 00:48:20 +0530

    All files added

---
+++ Services/BotSettings.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Solutions;
+using MyAssistant1.TokenExchange;
+
+namespace MyAssistant1.Services
+{
+    public class BotSettings : BotSettingsBase
+    {
+        public TokenExchangeConfig TokenExchangeConfig { get; set; }
+    }
+}
